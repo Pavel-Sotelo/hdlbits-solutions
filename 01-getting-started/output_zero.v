0062ca9ff0fdd4d7bd96db5f6f408db94228130f
Pavel-Sotelo <pavelsote990@gmail.com>
2026-04-23 15:53:34 -0700

    Add getting started: output_zero

---
+++ 01-getting-started/output_zero.v
@@ -0,0 +1,9 @@
+// Build a circuit with no inputs and one output that outputs a constant 0
+
+module top_module(
+    output zero
+);// Module body starts after semicolon
+	
+	// Fun fact: For Quartus synthesis, not assigning a value to a signal usually results in 0. This problem is actually easier than the previous one.
+
+endmodule
