68537c3308cf3899bf01b0cc18c39974f60b2a6b
Pavel-Sotelo <pavelsote990@gmail.com>
2026-04-29 14:22:10 -0700

    Add verilog-language: wire, and_gate

---
+++ and_gate.v
@@ -0,0 +1,8 @@
+module top_module   (input a,b, 
+                    output out);
+
+    //AND gate is the same as C. & opeates on each bit, && compares conditions
+
+    assign out = a & b;
+    
+endmodule
