6f67242627f996287f5fa40684d55f2c5c11d4c6
Pavel-Sotelo <pavelsote990@gmail.com>
2026-04-29 13:57:05 -0700

    Add comments to 4wires

--- 4wires.v
+++ 4wires.v
@@ -2,9 +2,12 @@ module top_module(
     input a,b,c,
     output w,x,y,z );
 
+//assign is a permanent connection, not a one-time copy
+//order of assign statements doesn't matter, all run simultaneously
+
 		assign w = a;
     	assign x = b;
     	assign y = b;
     	assign z = c;
-        
+
 endmodule
