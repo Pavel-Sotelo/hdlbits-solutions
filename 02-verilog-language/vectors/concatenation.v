7e03b1f4376cec9359350ab1f7d9857900718407
Pavel-Sotelo <pavelsote990@gmail.com>
2026-05-01 19:25:59 -0700

    Add verilog-language: wire, concatenation

---
+++ 02-verilog-language/vectors/concatenation.v
@@ -0,0 +1,11 @@
+
+////Concatenate six 5-bit inputs + two 1-bits (32 total), split into four 8-bit outputs
+
+module top_module (
+                    input [4:0] a, b, c, d, e, f,
+                    output [7:0] w, x, y, z 
+                    );
+
+    assign {w, x, y, z} = {a, b, c, d, e, f, 2'b11};
+
+endmodule
