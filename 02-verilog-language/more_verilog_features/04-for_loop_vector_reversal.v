f858786dddf5519661b9999c0333f57a5397d42c
Pavel-Sotelo <pavelsote990@gmail.com>
2026-05-14 14:48:57 -0700

    Add circuits: combinational logic section complete

--- 02-verilog-language/more_verilog_features/04-for_loop_vector_reversal.v
+++ 02-verilog-language/more_verilog_features/04-for_loop_vector_reversal.v
@@ -3,12 +3,11 @@
 module top_module( 
     input [99:0] in,
     output reg[99:0] out
-);
-    integer i;                       //out[0] = in[99]
-    								//out[1] = in[98]
+);              
+    						
     always @(*) begin
        
-        for(i = 0; i <= 99; i = i+1) begin
+        for(int i = 0; i < $bits(out); i = i+1) begin
 
             out[i] = in[99 - i];
             
